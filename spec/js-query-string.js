3dda3d2e7bf920625c1d131290ac97d522604343
Matt <mattc65@gmail.com>
2016-04-16 01:21:07 +0100

    Add functionality

---
+++ spec/js-query-string.js
@@ -0,0 +1,9 @@
+var querify = require("./lib/index.js");
+
+describe("Object -> Query String", function () {
+
+  it("Should work for objects with strings", function () {
+    var querystring = 
+  });
+
+});
